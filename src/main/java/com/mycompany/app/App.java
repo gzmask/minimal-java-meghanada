073d2242b51b12ca170ce5c1edd4f2fd29a9b558
Shulang Lei <shulang.lei@unbounce.com>
2017-08-10 14:49:57 -0700

    first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/** Hello world! */
+public class App {
+  private static final String hi() {
+    return "ehllo owlrkd";
+  }
+
+  //to run this, ~,x:~ exec:java
+  public static void main(String[] args) {
+    System.out.println(App.hi());
+  }
+}
